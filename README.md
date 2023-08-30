c467e1014a07812a1c072200e9240eee6eebf369
Turak Firas Azim <firas.a.turak@student.jyu.fi>
2023-08-31 00:23:25 +0300

    3rd commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Basis for calculations: https://www.investor.gov/financial-tools-calculators/calculators/compound-interest-calculator
+
+-Laskurin visualisoinnin skaala on n. kokonaisarvo 1-40 000 000. Tätä laajemmat visuaalisaatiot eivät näytä niin hyvältä.
--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,23 @@ const currency = "€"; // Could make this an input in future but may hinder UX 
 const resultMessage = "Loppusumma: ";
 let compoundInterestArray = []; //Data types are defined by pushes
 
+function validateForm() {
+  const monthlyValue = document.forms["values"]["monthly"];
+  const durationValue = document.forms["values"]["duration"];
+  const returnValue = document.forms["values"]["return"];
+
+  if (durationValue.valueAsNumber > 99) {
+    alert("Maksimaalinen sijoitusaika on 99 vuotta");
+    return false;
+  }
+  if(returnValue.valueAsNumber > 100) {
+    alert("Maksimaalinen vuosituotto on 100%");
+    return false;
+  }
+  if(monthlyValue.value === "") monthlyValue.value = 0;
+  calculateFV();
+}
+
 function calculateFV(){
     let initialInvestment = document.getElementById("initialInvestment").valueAsNumber;
     let monthlyInvestment = document.getElementById("monthlyInvestment").valueAsNumber;
@@ -10,8 +27,10 @@ function calculateFV(){
 
     const result = document.getElementById("result");
 
-    if(monthlyInvestment === "" || monthlyInvestment === "0") return resultMessage + (result.innerHTML = resultMessage + simpleRate(initialInvestment, annualRate, years).toFixed(2) + currency); //Make the input field always 0 if it is emptied
-    else return (result.innerHTML = resultMessage + compoundInterest(initialInvestment, monthlyInvestment, annualRate, years).toFixed(2) + currency);
+    if(monthlyInvestment === "0") return resultMessage + 
+      (result.innerHTML = resultMessage + simpleRate(initialInvestment, annualRate, years).toFixed(2) + currency); //Make the input field always 0 if it is emptied
+    else return (result.innerHTML = resultMessage + 
+      compoundInterest(initialInvestment, monthlyInvestment, annualRate, years).toFixed(2) + currency);
 }
 
 function simpleRate (investment, annualRate, years) {
@@ -37,7 +56,7 @@ function compoundInterest (initialInvestment, monthlyInvestment, annualRate, yea
         //How much interest in total by years
         interest = accumulatedValue - invested;
 
-        compoundInterestArray.push({years: "year " + i, accumulatedValue: accumulatedValue, invested: invested, interest: interest}); //accumulatedValue currently unused by array but remains for easy access if future needs
+        compoundInterestArray.push({years: "vuosi " + i, accumulatedValue: accumulatedValue, invested: invested, interest: interest}); //accumulatedValue currently unused by array but remains for easy access if future needs
     }
     
     doChart();
@@ -46,11 +65,11 @@ function compoundInterest (initialInvestment, monthlyInvestment, annualRate, yea
 
  function doChart() { 
   const yearValues = compoundInterestArray.map(year => year.years)
-  const interestValues = compoundInterestArray.map(year => year.interest);
-  const investedValues = compoundInterestArray.map(year => year.invested);
-  const accumulationValues = compoundInterestArray.map(year => year.accumulatedValue);
+  const interestValues = compoundInterestArray.map(year => year.interest.toFixed(2));
+  const investedValues = compoundInterestArray.map(year => year.invested.toFixed(2));
+  const accumulationValues = compoundInterestArray.map(year => year.accumulatedValue.toFixed(2));
   
-  new Chart("chart", {
+  const chart = new Chart("chart", {
     type: "bar",
     data: {
       labels: yearValues,
@@ -76,6 +95,7 @@ function compoundInterest (initialInvestment, monthlyInvestment, annualRate, yea
     },
     options: {
         responsive: true,
+        maintainAspectRatio:false,
         plugins: {
             legend: {
               position: 'top',
