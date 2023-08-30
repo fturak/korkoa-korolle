6791e78ad1c38a7be2183889d856e64afd53a822
Turak Firas Azim <firas.a.turak@student.jyu.fi>
2023-08-30 16:16:29 +0300

    2nd commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
-let currency = "€"; // Could make this an input in future but may hinder UX with complexity
-
-google.charts.load('current', {packages: ['corechart', 'bar']});
+const currency = "€"; // Could make this an input in future but may hinder UX with complexity
+const resultMessage = "Loppusumma: ";
+let compoundInterestArray = []; //Data types are defined by pushes
 
 function calculateFV(){
     let initialInvestment = document.getElementById("initialInvestment").valueAsNumber;
@@ -10,8 +10,8 @@ function calculateFV(){
 
     const result = document.getElementById("result");
 
-    if(monthlyInvestment === "" || monthlyInvestment === "0") return (result.innerHTML = simpleRate(initialInvestment, annualRate, years).toFixed(2) + currency); //Make the input field always 0 if it is emptied
-    else return (result.innerHTML = compoundInterest(initialInvestment, monthlyInvestment, annualRate, years).toFixed(2) + currency);
+    if(monthlyInvestment === "" || monthlyInvestment === "0") return resultMessage + (result.innerHTML = resultMessage + simpleRate(initialInvestment, annualRate, years).toFixed(2) + currency); //Make the input field always 0 if it is emptied
+    else return (result.innerHTML = resultMessage + compoundInterest(initialInvestment, monthlyInvestment, annualRate, years).toFixed(2) + currency);
 }
 
 function simpleRate (investment, annualRate, years) {
@@ -21,9 +21,6 @@ function simpleRate (investment, annualRate, years) {
 function compoundInterest (initialInvestment, monthlyInvestment, annualRate, years) {
     var monthlyRate = annualRate / 100 / 12; // In 0.45 format so is missing the 1 if used in calcs
     var months = 12;
-
-    var compoundInterestArray = []; //Data types are defined by pushes
-
     var accumulatedValue = 0;
 
     for (let i = 1; i <= years; i++) {
@@ -40,34 +37,61 @@ function compoundInterest (initialInvestment, monthlyInvestment, annualRate, yea
         //How much interest in total by years
         interest = accumulatedValue - invested;
 
-        compoundInterestArray.push({year: "year " + i, acummulation: accumulatedValue, invested: invested, interest: interest}); //accumulatedValue currently unused but remains for easy access if future needs
+        compoundInterestArray.push({years: "year " + i, accumulatedValue: accumulatedValue, invested: invested, interest: interest}); //accumulatedValue currently unused by array but remains for easy access if future needs
     }
     
-    console.log(compoundInterestArray)
-    drawAxisTickColors(compoundInterestArray);
-
+    doChart();
     return accumulatedValue;
 }
 
-function drawAxisTickColors(chartsdata) {
-      
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'year');
-      data.addColumn('number', 'Sijoitettu pääoma');
-      data.addColumn('number', 'Tuotto');
-      
-      for (let i = 0; i < chartsdata.length; i++) {
-          data.addRow([chartsdata[i].year, chartsdata[i].invested, chartsdata[i].interest]);
+ function doChart() { 
+  const yearValues = compoundInterestArray.map(year => year.years)
+  const interestValues = compoundInterestArray.map(year => year.interest);
+  const investedValues = compoundInterestArray.map(year => year.invested);
+  const accumulationValues = compoundInterestArray.map(year => year.accumulatedValue);
+  
+  new Chart("chart", {
+    type: "bar",
+    data: {
+      labels: yearValues,
+      datasets: [
+        {
+            label: "Sijoitettu pääoma",
+            backgroundColor: "#00A45E",
+            data: investedValues
+        },    
+        {
+            label: "Tuotto",
+            backgroundColor: "#00B625",
+            data: interestValues
+        },
+        {
+            label: "Kokonaisarvo",
+            backgroundColor: "#009CA9",
+            borderColor: "#009CA9",
+            data: accumulationValues,
+            type: "line",
+        }
+      ]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+              position: 'top',
+            }
+        },
+        scales: {
+          x: {
+            stacked: true,
+            grid: {
+              display: false
+            }
+          },
+          y: {
+            stacked: true,
+          }
+        }
       }
-
-      var options = {
-        width: 800,
-        height: 400,
-        legend: { position: 'top', maxLines: 3 },
-        bar: { groupWidth: '85%' },
-        isStacked: true,
-      };
-
-      var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
-    }
+  });
+}
